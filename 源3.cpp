66ef66d60e33688a81166d4f5568530234b574f5
zhoumaozhang <71367170+zhoumaozhang@users.noreply.github.com>
2020-10-21 09:29:35 +0800

    Add files via upload

--- 源3.cpp
+++ 源3.cpp
@@ -13,7 +13,7 @@ int main()
 	bitwise_not(result1, result);//颜色反转
 	cv::imshow("result", result);
 
-	Mat element = getStructuringElement(MORPH_RECT, Size(1, 1));
+	Mat element = getStructuringElement(MORPH_RECT, Size(3, 3));
 	cv::Mat result_open;
 	morphologyEx(result, result_open, MORPH_OPEN, element);
 	cv::imshow("result_open", result_open);
@@ -21,7 +21,22 @@ int main()
 
 	cv::Mat Imglabels, Imgstats, Imgcentriods;
 	int Imglabelnum = connectedComponentsWithStats(result_open, Imglabels, Imgstats, Imgcentriods);
-	cout << Imglabelnum ;
+
+	int num = 0;
+	for (int i = 0; i < Imglabelnum; i++)
+	{
+		double height = Imgstats.at<int>(i, CC_STAT_HEIGHT);
+		cout << height << '\n';
+
+		if (height < 300)
+		{
+			num++;
+		}
+	}
+	
+	//连通域的高
+	cout << "个数为" << num;
+
 
 	waitKey(0);
 	return 0;

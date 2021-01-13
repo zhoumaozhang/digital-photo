acbbeb2d3d2b4e7d85a1276afa36ebdae1db3e10
zhoumaozhang <71367170+zhoumaozhang@users.noreply.github.com>
2021-01-13 11:32:56 +0800

    Add files via upload

---
+++ parameters.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		0			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"H:\\vtest.avi"		
+#define OPENPOSE_VIDEO		"D:/2020/课程/数字图像处理/上课编程/15/dip-class-demos-feature15.DeepNeuralNetwork/dip-class-demos-feature15.DeepNeuralNetwork/openposeTest.mp4"		
+
+//#define INPUT_METHOD									//0: image 1:video 2:camera
